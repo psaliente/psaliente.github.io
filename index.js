48a0bb7295a736d306f7c7c76e74ffd1d8d40fe9
Peter John Saliente <psaliente@allegromicro.com>
2021-09-27 16:10:20 +0800

    updated lorem ipsum with real descriptions

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var Portfolio = function (_React$Component) {
 
         _this.state = {
             Me: 'Peter John C. Saliente',
-            Description: 'Hi, welcome to my online portfolio where you can see the list of projects that I have participated during the span my software development and engineering career. I had been in the IT Industry since 2011 and then went on to do software development three years later.',
+            Description: 'Hi, welcome to my online portfolio where you can see the list of projects that I have participated throughout the span my software development and engineering career. I had been in the IT Industry since 2011 and then went on to do software development three years later.',
             Projects: [{
                 Title: 'Active Directory Web API',
                 Description: 'A Web API that provides a common source of Active Directory Information that can easily be consumed by other Applications',
@@ -57,31 +57,31 @@ var Portfolio = function (_React$Component) {
                 Languages: ['T-SQL']
             }, {
                 Title: 'EPMAP',
-                Description: 'A SharePoint web application used for the approval of promotional materials and events',
+                Description: 'A SharePoint WebPart-based application used for the approval of promotional materials and events',
                 Languages: ['SharePoint', 'C#', 'Web API', 'JavaScript', 'AngularJS', 'SQL']
             }, {
                 Title: '4Runner',
-                Description: 'Lorem ipsum dolor sit amet adipiscing elit consectitur',
+                Description: 'A SharePoint WebPart-based application',
                 Languages: ['SharePoint', 'C#', 'Web API', 'JavaScript']
             }, {
                 Title: 'AIG',
-                Description: 'Account-in-a-Glance',
+                Description: 'Account-in-a-Glance, is a SharePoint WebPart-based web app dashboard for a quick overview of the sales and costs of product in a certain sales territory',
                 Languages: ['SharePoint', 'C#', 'JavaScript', 'KnockOutJS']
             }, {
                 Title: 'ePAF',
-                Description: 'Electronic Principal Approval Form',
+                Description: 'Electronic Principal Approval Form, is a SharePoint WebPart-based application for the basic record keeping of Principal Approval Forms',
                 Languages: ['SharePoint', 'C#', 'REST API', 'JavaScript', 'KnockOutJS']
             }, {
                 Title: 'CRAMS',
-                Description: 'Crew Resource Allocation Management System',
+                Description: 'Crew Resource Allocation Management System - is a SharePoint WebPart-Based Application used by airline mechanics to manage their aircraft maintenance schedules and tasks',
                 Languages: ['SharePoint', 'C#', 'REST API', 'JavaScript', 'KnockOutJS']
             }, {
                 Title: 'Flight Status Checker',
-                Description: 'Lorem ipsum dolor sit amet adipiscing elit consectitur',
+                Description: 'A simple SharePoint WebPart to view the flight status of incoming aircrafts',
                 Languages: ['SharePoint', 'C#', 'HTTP Handler', 'JavaScript']
             }, {
                 Title: 'Cabin Crew Leave Exchange Form',
-                Description: 'Lorem ipsum dolor sit amet adipiscing elit consectitur',
+                Description: 'A simple SharePoint WebPart for flight attendants to submit Leave Exchage Request',
                 Languages: ['SharePoint', 'C#', 'JavaScript']
             }],
             Skills: [{
