1e7f9ab114832d5859ab9ba152a2afe6be42dd8f
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2024-11-11 20:38:11 +0800

    #NONE Comment - adjusted background image styling

--- src/styles/portfolio.style.ts
+++ src/styles/portfolio.style.ts
@@ -4,13 +4,11 @@ import { ai_bg } from "../assets/images"
 const useStyle = () => {
     return {
         PortfolioPage: {
-            background: "linear-gradient(rgba(243,243,245), rgba(123,123,123))",
-            backgroundColor: "#f3f3fe",
             paddingBottom: 60,
             backgroundImage: `url(${ai_bg})`,
             backgroundRepeat: 'no-repeat',
             backgroundAttachment: 'fixed',
-            backgroundSize: 'cover',
+            backgroundSize: 'contain',
         }
     }
 }
