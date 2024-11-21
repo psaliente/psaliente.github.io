6a1ab213c861326db4241e0507082bb1ea24ef69
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2024-11-21 18:50:59 +0800

    NONE #Comment - migrated header component

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,20 +1,11 @@
 import { MyInfoType } from "../types/MyInfo.type";
-import useStyle from "../styles/header.style";
 import ScrollAnimation from "react-animate-on-scroll";
 
 const HeaderComponent = ({Name, Description}: MyInfoType) => {
-    const styles = useStyle();
-
     return (<ScrollAnimation animateIn="fadeInDownBig">
-        <div style={styles.Header}>
-        <h1 className="display-1 text-center" style={styles.Title}>{Name}</h1>
-        <div className="container">
-          <div className="row">
-            <div className="col-xs-12">
-              <p className="lead" style={styles.Description}>{Description}</p>
-            </div>
-          </div>
-        </div>
+      <div className="flex flex-col gap-16 p-20 bg-gradient-to-t from-gray-100/95 to-sky-50/60">
+        <h1 className="text-7xl text-center font-[SourceCodePro] font-thin text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500">{Name}</h1>
+        <p className="text-4xl text-justify font-[SourceCodePro] text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-fuchsia-500">{Description}</p>
       </div>
     </ScrollAnimation>)
 }
