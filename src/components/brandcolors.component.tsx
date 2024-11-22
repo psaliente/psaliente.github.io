4400317d4474c3151c3c483371dc9a86a63a4ffc
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2024-11-22 17:09:15 +0800

    added animated brand colors glow

---
+++ src/components/brandcolors.component.tsx
@@ -0,0 +1,12 @@
+
+function BrandColors(): JSX.Element {
+    return (<div className="flex">
+        <div className="block relative w-full h-16 overflow-hidden">
+            <div className="bg-sky-300 w-2/3 h-12 rounded-[50%] absolute -top-2/3 left-0 blur-xl animate-pulse"></div>
+            <div className="bg-blue-400 w-2/3 h-12 rounded-[50%] absolute -top-1/2 left-[20%] blur-xl animate-pulse"></div>
+            <div className="bg-green-300 w-2/3 h-12 rounded-[50%] absolute -top-2/3 left-1/3 blur-xl animate-pulse"></div>
+        </div>
+    </div>)
+}
+
+export default BrandColors;
--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,12 +1,14 @@
 import { MyInfoType } from "../types/MyInfo.type";
 import ScrollAnimation from "react-animate-on-scroll";
+import BrandColors from "./brandcolors.component";
 
 const HeaderComponent = ({Name, Description}: MyInfoType) => {
     return (<ScrollAnimation animateIn="fadeInDownBig">
-      <div className="flex flex-col gap-16 bg-gradient-to-t from-gray-100/95 to-sky-50/60 p-10 md:p-20 ">
+      <div className="flex flex-col gap-16 border bg-gradient-to-t from-gray-100 to-sky-50/60 p-10 md:p-20 ">
         <h1 className="p-1 text-5xl md:text-7xl text-center font-[SourceCodePro] font-thin text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-sky-500 to-green-500">{Name}</h1>
         <p className="p-1 text-2xl md:text-4xl text-justify font-[SourceCodePro] text-gray-500">{Description}</p>
       </div>
+      <BrandColors />
     </ScrollAnimation>)
 }
 
