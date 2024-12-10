c52194e3272c43619e0f603c6f13eed7e2a14a6f
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2024-12-10 21:20:22 +0800

    NONE #Comment - added SVG resources

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,34 @@
-import ai_bg from '../images/ai_bg.png';
+import angularSVG from '../images/angular.svg';
+import appIconConsole from '../images/app-icon-console.svg';
+import appIconCrossPlatform from '../images/app-icon-cross-platform.svg';
+import appIconDesktop from '../images/app-icon-desktop.svg';
+import appIconGeneric from '../images/app-icon-generic.svg';
+import appIconMobile from '../images/app-icon-mobile.svg';
+import appIconWeb from '../images/app-icon-web.svg';
+import bootstrapSVG from '../images/bootstrap.svg';
+import csharpSVG from '../images/C_sharp.svg';
+import cssSVG from '../images/css.svg';
+import html5SVG from '../images/html5.svg';
+import jsSVG from '../images/js.svg';
+import reactSVG from '../images/react.svg';
+import tailwindSVG from '../images/tailwind.svg';
+import tsSVG from '../images/ts.svg';
+
 
 export {
-    ai_bg
+    angularSVG,
+    appIconConsole,
+    appIconCrossPlatform,
+    appIconDesktop,
+    appIconGeneric,
+    appIconMobile,
+    appIconWeb,
+    bootstrapSVG,
+    csharpSVG,
+    cssSVG,
+    html5SVG,
+    jsSVG,
+    reactSVG,
+    tailwindSVG,
+    tsSVG
 }
