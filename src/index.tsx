974fa52236c0f89f970c225fd5876dbdb43c70e6
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2024-12-17 17:57:09 +0800

    NONE #Comment - duplicate declaration of tailwind css;

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app";
 import reportWebVitals from "./reportWebVitals";
 import "animate.css/animate.compat.css";
+import './index.css';
 import './assets/app.scss';
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
