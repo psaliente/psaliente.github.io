b44396c622882f184e7e00f7c64cace8014a73e8
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2024-12-24 16:04:02 +0800

    NONE #Comment - footer styling adjustments

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -1,9 +1,10 @@
 export default function Footer() {
-    return <div className="flex flex-col p-6 gap-2 text-center">
+    return <div className="flex flex-col p-6 pt-20 gap-2 text-center">
         <div className="flex flex-row flex-wrap gap-3 w-full items-center justify-center">
             <a href="https://github.com/psaliente">My Github Profile</a>
             <a href="https://www.linkedin.com/in/peter-saliente-2a4aa942/">My LinkedIn Profile</a>
         </div>
+        <hr className="w-2/3 self-center border-gray-500" />
         <span>Peter John C. Saliente - 2021</span>
     </div>
 }
