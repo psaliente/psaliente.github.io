a0dee0a1ea0eefaaaad1ffe1f6aac1da9a904647
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2024-12-24 16:09:30 +0800

    NONE #Comment - added contact email

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -3,6 +3,7 @@ export default function Footer() {
         <div className="flex flex-row flex-wrap gap-3 w-full items-center justify-center">
             <a href="https://github.com/psaliente">My Github Profile</a>
             <a href="https://www.linkedin.com/in/peter-saliente-2a4aa942/">My LinkedIn Profile</a>
+            <a href="mailto:pjc.saliente@gmail.com">My Contact Email</a>
         </div>
         <hr className="w-2/3 self-center border-gray-500" />
         <span>Peter John C. Saliente - 2021</span>
