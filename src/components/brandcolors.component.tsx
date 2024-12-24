a041ccbd31fae542a7b42ed0adbe01ca8faecd42
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2024-12-24 15:53:28 +0800

    NONE #Comment - color adjustments

--- src/components/brandcolors.component.tsx
+++ src/components/brandcolors.component.tsx
@@ -3,9 +3,9 @@ import React from "react";
 function BrandColors() {
     return (<div className="flex">
         <div className="block relative w-full h-16 overflow-hidden">
-            <div className="bg-sky-300 w-2/3 h-12 rounded-[50%] absolute -top-2/3 left-0 blur-xl animate-pulse"></div>
-            <div className="bg-blue-400 w-2/3 h-12 rounded-[50%] absolute -top-1/2 left-[20%] blur-xl animate-pulse"></div>
-            <div className="bg-green-300 w-2/3 h-12 rounded-[50%] absolute -top-2/3 left-1/3 blur-xl animate-pulse"></div>
+            <div className="bg-sky-400 w-2/3 h-12 rounded-[50%] absolute -top-2/3 left-0 blur-lg animate-pulse"></div>
+            <div className="bg-lime-400 w-2/3 h-12 rounded-[50%] absolute -top-2/3 left-1/3 blur-lg animate-pulse"></div>
+            <div className="bg-cyan-400 w-2/3 h-12 rounded-[50%] absolute -top-1/2 left-[20%] blur-lg animate-pulse"></div>
         </div>
     </div>)
 }
--- src/components/labels/textGradient.component.tsx
+++ src/components/labels/textGradient.component.tsx
@@ -6,8 +6,8 @@ type TextGradientPropsType = {
     toColor?: string
 } & React.PropsWithChildren
 
-function TextGradient({children, fromColor = 'from-blue-500', viaColor = 'via-sky-500', toColor = 'to-green-500'}: TextGradientPropsType) {
-    return <span className={`font-[SourceCodePro] font-thin text-transparent bg-clip-text bg-gradient-to-r ${fromColor} ${viaColor} ${toColor}`}>{children}</span>
+function TextGradient({children, fromColor = 'from-sky-500', viaColor = 'via-cyan-500', toColor = 'to-lime-500'}: TextGradientPropsType) {
+    return <span className={`font-[SourceCodePro] font-thin text-transparent bg-clip-text bg-gradient-to-r ${fromColor} from-10% ${viaColor} via-60% ${toColor} to-90%`}>{children}</span>
 }
 
 export default React.memo(TextGradient);
