126a854d5087632e0ede1647f868dd4b6973dab9
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2025-01-09 18:46:48 +0800

    NONE #Comment - added official links

--- src/components/proglangs.component.tsx
+++ src/components/proglangs.component.tsx
@@ -9,22 +9,34 @@ export default function ProgLangs() {
         </Section.Title>
         <Section.Content className="flex flex-row flex-wrap p-4 hd:p-8 gap-6 justify-center">
             <Card className="content-center p-6 rounded-xl bg-slate-200">
-                <img src={csharpSVG} className="h-20" alt="C# Logo"/>
+                <a href="https://learn.microsoft.com/en-us/dotnet/csharp/">
+                    <img src={csharpSVG} className="h-20" alt="C# Logo"/>
+                </a>
             </Card>
             <Card className="content-center p-6 rounded-xl bg-slate-200">
-                <img src={cssSVG} className="h-20" alt="CSS Logo"/>
+                <a href="https://www.w3.org/Style/CSS/Overview.en.html">
+                    <img src={cssSVG} className="h-20" alt="CSS Logo"/>
+                </a>
             </Card>
             <Card className="content-center p-6 rounded-xl bg-slate-200">
-                <img src={jsSVG} className="h-20" alt="JavaScript Logo"/>
+                <a href="https://www.javascript.com/">
+                    <img src={jsSVG} className="h-20" alt="JavaScript Logo"/>
+                </a>
             </Card>
             <Card className="content-center p-6 rounded-xl bg-slate-200">
-                <img src={html5SVG} className="h-20" alt="HTML5 Logo"/>
+                <a href="https://html.com/">
+                    <img src={html5SVG} className="h-20" alt="HTML5 Logo"/>
+                </a>
             </Card>
             <Card className="content-center p-6 rounded-xl bg-slate-200">
-                <img src={sqlSVG} className="h-20" alt="SQL Logo"/>
+                <a href="https://www.iso.org/standard/76583.html">
+                    <img src={sqlSVG} className="h-20" alt="SQL Logo"/>
+                </a>
             </Card>
             <Card className="content-center p-6 rounded-xl bg-slate-200">
-                <img src={tsSVG} className="h-20" alt="TypeScript Logo"/>
+                <a href="https://www.typescriptlang.org/">
+                    <img src={tsSVG} className="h-20" alt="TypeScript Logo"/>
+                </a>
             </Card>
         </Section.Content>
     </Section>
