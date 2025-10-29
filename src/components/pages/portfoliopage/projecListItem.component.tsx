fa81099be4e58156e8a83a18b24d480b964d75f5
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2025-10-29 17:36:48 +0800

    simplified project item card navigation

--- src/components/pages/portfoliopage/projecListItem.component.tsx
+++ src/components/pages/portfoliopage/projecListItem.component.tsx
@@ -16,34 +16,23 @@ function ProjectListItem(projectItem: ProjectListItemPropType) {
   const { setProjectItem } = useContext(ProjectItemContext);
   const projectThumb = useProjectThumbnail(projectItem.Type);
   const trimmedTitle = trimText2Elipse(projectItem.Title, 30);
-  const trimmedDesc = trimText2Elipse(projectItem.Description, 60);
 
   const setCurrentProjectItem = () => {
     setProjectItem(projectItem);
   };
 
   return (
-    <Card className="flex flex-row flex-wrap gap-4 px-7 pb-5 pt-8 bg-white dark:bg-gray-800 justify-center hd:justify-normal transition delay-300 duration-500 hover:scale-[1.02]">
-      <img src={projectThumb} className="w-40 h-40" alt="mobile app" />
-      <div className="flex flex-col gap-5 max-w-xs content-between">
-        {projectItem.Link ? (
-          <Link to={projectItem.Link}>
-            <h3 className="text-3xl md:text-5xl font-[SourceCodePro] text-blue-500 dark:text-blue-300 text-wrap break-words">
-              {trimmedTitle}
-            </h3>
-          </Link>
-        ) : (
+    <Link to="/project" onClick={setCurrentProjectItem}>
+      <Card className="flex flex-row flex-wrap gap-4 px-7 pb-5 pt-8 bg-white dark:bg-gray-800 justify-center hd:justify-normal transition delay-300 duration-500 hover:scale-[1.02]">
+        <img src={projectThumb} className="w-40 h-40" alt={projectItem.Type} />
+        <div className="flex flex-col gap-5 max-w-xs content-between">
           <h3 className="dark:text-gray-300 text-3xl md:text-5xl font-[SourceCodePro] text-wrap break-words">
             {trimmedTitle}
           </h3>
-        )}
-        <LanguageList ProjectID={'' + projectItem.ProjectID} Languages={projectItem.Languages} />
-        <p className="text-lg md:text-xl font-[SourceCodePro] text-gray-700 dark:text-gray-200">{trimmedDesc}</p>
-        <Link to="/project" className="font-bold text-blue-700 dark:text-blue-500" onClick={setCurrentProjectItem}>
-          see my involvements
-        </Link>
-      </div>
-    </Card>
+          <LanguageList ProjectID={'' + projectItem.ProjectID} Languages={projectItem.Languages} />
+        </div>
+      </Card>
+    </Link>
   );
 }
 
