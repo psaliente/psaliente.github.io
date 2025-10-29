bfce2bdb518e32544fb1107a7477e625e31d4e5a
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2025-10-29 15:08:55 +0800

    remove unused pages

--- src/components/containers/card.component.tsx
+++ src/components/containers/card.component.tsx
@@ -5,7 +5,11 @@ type CardPropsType = {
 } & React.PropsWithChildren;
 
 function Card({ className, children }: CardPropsType) {
-  return <div className={'z-10 rounded-3xl shadow-lg hover:shadow-xl ' + className}>{children}</div>;
+  return (
+    <div className={'z-10 rounded-3xl shadow-lg hover:shadow-xl scale-100 hover:scale-105 ' + className}>
+      {children}
+    </div>
+  );
 }
 
 export default React.memo(Card);
--- src/components/router.component.tsx
+++ src/components/router.component.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, HashRouter } from 'react-router-dom';
 import { PortfolioPage } from './pages/portfoliopage';
 import { ProjectItemPage } from './pages/projectitempage';
-import { DecodePage, DecryptPage, EncodePage, EncryptPage, JulianConverterPage, ToolsPage } from './pages/toolspage';
+import { DecodePage, EncodePage, JulianConverterPage, ToolsPage } from './pages/toolspage';
 
 const RouterComponent = () => (
   <HashRouter>
@@ -9,8 +9,6 @@ const RouterComponent = () => (
       <Route path="/" element={<PortfolioPage />}></Route>
       <Route path="/project" element={<ProjectItemPage />}></Route>
       <Route path="/tools" element={<ToolsPage />}></Route>
-      <Route path="/tools/decrypt" element={<DecryptPage />}></Route>
-      <Route path="/tools/encrypt" element={<EncryptPage />}></Route>
       <Route path="/tools/decode" element={<DecodePage />}></Route>
       <Route path="/tools/encode" element={<EncodePage />}></Route>
       <Route path="/tools/julian-convert" element={<JulianConverterPage />}></Route>
