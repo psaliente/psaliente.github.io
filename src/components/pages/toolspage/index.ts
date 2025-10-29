43279bf23af143d3aac4591de9d3c59ab414fefe
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2025-10-29 15:09:10 +0800

    removed unused pages

--- src/components/pages/toolspage/index.ts
+++ src/components/pages/toolspage/index.ts
@@ -1,8 +1,6 @@
-import { EncryptPage } from './encryptpage';
-import { DecryptPage } from './decryptpage';
 import { EncodePage } from './encodepage';
 import { DecodePage } from './decodepage';
 import { JulianConverterPage } from './julianconverterpage';
 import ToolsPage from './toolsPage.component';
 
-export { EncryptPage, DecryptPage, EncodePage, DecodePage, JulianConverterPage, ToolsPage };
+export { EncodePage, DecodePage, JulianConverterPage, ToolsPage };
