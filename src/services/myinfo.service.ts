5456e2bbca0e8b51308bc7706161c95264c3e1b1
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2025-10-30 10:20:31 +0800

    NONE #comment - minor update on portfolio description

--- src/services/myinfo.service.ts
+++ src/services/myinfo.service.ts
@@ -5,7 +5,7 @@ export const useMyInfoService = () => {
     return {
       Name: '<Peter John C. Saliente/>',
       Description:
-        'Hello and welcome to my online portfolio! Here, you’ll find a curated list of projects I’ve been involved in throughout my software development and engineering career. My journey in the IT industry began back in 2011, and three years later, I delved into the exciting world of software development.'
+        'Hello and welcome to my growing online portfolio! Here, you’ll find a curated list of projects I’ve been involved in throughout my software development and engineering career. My journey in the IT industry began back in 2011, and three years later, I delved into the exciting world of software development.'
     };
   }
 
--- src/types/Project.Type.ts
+++ src/types/Project.Type.ts
@@ -2,6 +2,7 @@ export type ProjectType = {
   Title: string;
   Description: string;
   Type?: 'console' | 'cross-platform' | 'desktop' | 'generic' | 'mobile' | 'web';
+  DevType?: 'fe' | 'be' | 'data' | 'full';
   Link?: string;
   Role: string[];
   Languages: string[];
