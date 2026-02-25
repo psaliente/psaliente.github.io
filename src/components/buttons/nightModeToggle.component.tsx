0accac8b2f5b3e07e52ab231547d7e026d0a3108
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2026-02-25 17:09:22 +0800

    styling fixes post-upgrade

--- src/components/buttons/nightModeToggle.component.tsx
+++ src/components/buttons/nightModeToggle.component.tsx
@@ -9,7 +9,7 @@ export default function NightModeToggle() {
       onClick={toggleTheme}
       className="self-center font-[SourceCodePro] bg-gray-300 dark:bg-gray-800 transition delay-300 duration-500 rounded-xl overflow-hidden"
     >
-      <div className="flex flex-row gap-4 text-lg md:text-2xl drop-shadow-lg dark:drop-shadow-white">
+      <div className="flex flex-row gap-4 text-lg md:text-2xl drop-shadow-lg">
         <div className="-z-10 rounded-xl bg-white dark:bg-gray-600 dark:translate-x-full w-1/2 h-full absolute transition duration-1000"></div>
         <div className="z-10 py-2 px-6 text-center text-gray-800 dark:text-gray-600 transition delay-300 duration-500">
           Light Mode
