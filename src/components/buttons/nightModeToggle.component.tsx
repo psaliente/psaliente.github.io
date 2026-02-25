021241752ac9f01d5aeec775bd96ae259366ba30
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2026-02-25 19:31:54 +0800

    added inset

--- src/components/buttons/nightModeToggle.component.tsx
+++ src/components/buttons/nightModeToggle.component.tsx
@@ -7,14 +7,14 @@ export default function NightModeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className="self-center font-[SourceCodePro] bg-gray-300 dark:bg-gray-800 transition delay-300 duration-500 rounded-xl overflow-hidden"
+      className="self-center font-[SourceCodePro] bg-gray-300 dark:bg-gray-800 inset-shadow-sm/30 dark:inset-shadow-sm/90 transition delay-300 duration-500 rounded-xl overflow-hidden"
     >
-      <div className="flex flex-row gap-4 text-lg md:text-2xl drop-shadow-lg">
+      <div className="flex flex-row gap-4 text-lg md:text-2xl drop-shadow-sm/50 dark:drop-shadow-sm/90">
         <div className="-z-10 rounded-xl bg-white dark:bg-gray-600 dark:translate-x-full w-1/2 h-full absolute transition duration-1000"></div>
-        <div className="z-10 py-2 px-6 text-center text-gray-800 dark:text-gray-600 transition delay-300 duration-500">
+        <div className="z-10 py-2 px-6 text-center text-gray-800 dark:text-gray-400 transition delay-300 duration-500">
           Light Mode
         </div>
-        <div className="z-10 py-2 px-6 text-center text-gray-100 dark:text-white transition delay-300 duration-500">
+        <div className="z-10 py-2 px-6 text-center text-gray-600 dark:text-white transition delay-300 duration-500">
           Night Mode
         </div>
       </div>
