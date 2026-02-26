3138c1ed7eea8a74a8e0f8a145afe977e5cc4b71
Peter John Saliente <peterjohncorderosaliente@gmail.com>
2026-02-26 14:38:17 +0800

    change project list to carousel

--- src/components/pages/portfoliopage/projecListItem.component.tsx
+++ src/components/pages/portfoliopage/projecListItem.component.tsx
@@ -21,8 +21,10 @@ function ProjectListItem(projectItem: ProjectListItemPropType) {
 
   return (
     <Link to="/project" onClick={setCurrentProjectItem}>
-      <Card className="flex flex-row flex-wrap gap-4 px-7 pb-5 pt-8 bg-white dark:bg-gray-800 justify-center 2xs:justify-normal transition delay-300 duration-500 hover:scale-[1.02]">
-        <img src={projectThumb} className="w-40 h-40" alt={projectItem.Type} />
+      <Card className="w-sm h-(--project-card-height) gap-4 px-7 pb-5 pt-8 bg-white dark:bg-gray-800 justify-center transition delay-300 duration-500 hover:scale-[1.02]">
+        <div className="w-full flex justify-center">
+          <img src={projectThumb} className="w-40 h-40" alt={projectItem.Type} />
+        </div>
         <div className="flex flex-col gap-5 max-w-xs content-between">
           <h3 className="dark:text-gray-300 transition delay-300 duration-500 text-3xl md:text-5xl font-[SourceCodePro] text-wrap wrap-break-word">
             {trimmedTitle}
--- src/components/pages/portfoliopage/projectList.component.tsx
+++ src/components/pages/portfoliopage/projectList.component.tsx
@@ -9,16 +9,40 @@ type ProjectListPropType = { projects: ProjectType[] };
 function ProjectList({ projects }: ProjectListPropType) {
   const { TITLE, SUBTITLE } = SECTIONS.PROJECTS;
 
+  const scrollToLeft = () => {
+    const projectList = document.querySelector('.project-list');
+    if (projectList) {
+      projectList.scrollBy({ left: -300, behavior: 'smooth' });
+    }
+  };
+
+  const scrollToRight = () => {
+    const projectList = document.querySelector('.project-list');
+    if (projectList) {
+      projectList.scrollBy({ left: 300, behavior: 'smooth' });
+    }
+  };
+
   return (
     <Section className="flex flex-col gap-10 px-10 py-12 overflow-x-hidden">
       <Section.Title className="text-gray-500 dark:text-gray-100 transition delay-300 duration-500 font-thin wrap-break-word">
         <Section.Title.Gradient>{TITLE}</Section.Title.Gradient>
         {SUBTITLE}
       </Section.Title>
-      <Section.Content className="grid grid-cols-1 gap-x-8 gap-y-12 lg:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4">
-        {projects.map((p, i) => (
-          <ProjectListItem key={'projectItem' + i} ProjectID={i} {...p} />
-        ))}
+      <Section.Content className="flex flex-col gap-2">
+        <div className="project-list flex flex-row h-(--project-section-height) gap-8 overflow-x-hidden">
+          {projects.map((p, i) => (
+            <ProjectListItem key={'projectItem' + i} ProjectID={i} {...p} />
+          ))}
+        </div>
+        <div className="flex flex-row gap-3 justify-end">
+          <button className="rounded-full bg-white size-12 drop-shadow-sm" onClick={scrollToLeft}>
+            &lt;
+          </button>
+          <button className="rounded-full bg-white size-12 drop-shadow-sm" onClick={scrollToRight}>
+            &gt;
+          </button>
+        </div>
       </Section.Content>
     </Section>
   );
